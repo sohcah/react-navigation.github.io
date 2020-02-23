fe9bd7fbd6f47d113a1a957e999d85d75b37145f
satya164 <satyajit.happy@gmail.com>
2020-02-23 04:30:35 +0000

    Deploy website
    Deploy website version based on 551a214c80343b6797b9f6f8c9fa1b82d435ef57

--- js/snack.js
+++ js/snack.js
@@ -23,18 +23,11 @@ const DEPS_VERSIONS = {
   next: [],
 };
 
-function getVersion(){
-  const navBarItems = document.getElementsByClassName('navbar__items');
-  if(navBarItems[0] && navBarItems[0].children[2] && navBarItems[0].children[2].textContent){
-    return navBarItems[0].children[2].textContent;
-  }
-  return 'next';
-}
 // todo: should get the version somewhere, maybe within the page html,
 // and match the appropriate version of react-navigation/stack/tabs/drawer
 // based on that
 function getSnackUrl(options) {
-  let currentVersion = getVersion();
+  let currentVersion = document.querySelector('header a h3').innerText;
   let label = options.label || document.title;
   let code = options.code;
   let templateId = options.templateId;
@@ -66,15 +59,14 @@ function findNearestPreElement(node) {
   }
 
   while (nextElement) {
-    if(nextElement.tagName === 'DIV' && nextElement.className.includes('mdxCodeBlock_node_modules-@docusaurus-theme-classic-src-theme-MDXComponents-')){
-      nextElement = nextElement.firstChild;
-    }
     if (
       nextElement.tagName === 'PRE' &&
-      nextElement.children[1] &&
-      nextElement.children[1].tagName === 'CODE'
+      nextElement.firstChild &&
+      nextElement.firstChild.tagName === 'CODE' &&
+      (nextElement.firstChild.className.includes('language-js') ||
+        nextElement.firstChild.className.includes('language-ts'))
     ) {
-      return nextElement.children[1];
+      return nextElement;
     } else {
       nextElement = nextElement.nextElementSibling;
     }
@@ -105,7 +97,6 @@ function appendSnackLink() {
     let label = samp.innerText;
     let templateId = samp.getAttribute('id');
 
-
     let link = document.createElement('a');
     link.className = 'snack-sample-link';
     link.dataset.snack = true;
@@ -117,7 +108,6 @@ function appendSnackLink() {
       link.innerHTML = `Try this example on Snack ${openIcon}`;
     }
 
-
     // Add the href and append the link element if we have some code
     let href = getSnackUrl({ code, templateId, label });
 
@@ -126,7 +116,6 @@ function appendSnackLink() {
     }
 
     link.href = href;
-
     pre.insertAdjacentElement('afterend', link);
 
     // Don't try to add the link more than once!
