fe619126c334566e78e7ccf6f2596b13ed16ce9a
brentvatne <satyajit.happy@gmail.com>
2020-02-27 21:54:00 +0000

    Deploy website version based on 91c94163033c2a62ae0db860d1a4a5d25f42c9d8

---
+++ 5cf82627.27c3726f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[133],{256:function(n){n.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"announcement","name":"announcement","count":8,"permalink":"/blog/tags/announcement"}')}}]);
